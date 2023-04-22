e692474c4acefde37ea7f68a6343665a1c4260a8
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-04-22 19:36:52 +0000

    update - task 0 fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
 export default function getResponseFromAPI() {
   const success = true;
   return new Promise((resolve, reject) => {
-    success ? resolve() : reject();
+    const result = success ? resolve() : reject();
+    return result;
   });
 }

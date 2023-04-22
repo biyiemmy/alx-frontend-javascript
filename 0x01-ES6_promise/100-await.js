6fc5bb237c9138f550da0a60c27a064c769cd49c
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-04-22 21:14:45 +0000

    advanced task passed

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default const asyncUploadUser = async () => {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+};

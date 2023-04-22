279c9361915381b0d89de917559985839fc56331
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-04-22 20:04:29 +0000

    add: task 1 passed

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,14 @@
-default export 
+export default function getFullResponseFromAPI(boolean) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}

b17a428a840a71c7d3bff86ee0222363cd9dcc8c
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-04-28 19:09:03 +0000

    update: implementation of the StudentClass class and the corresponding interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,31 @@ interface Directors extends Teacher {
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
+
+interface IStudent {
+  firstName: string;
+  lastName: string;
+}
+
+interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudent, IStudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
